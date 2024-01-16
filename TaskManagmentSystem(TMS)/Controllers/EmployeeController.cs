e8f4a10a0323242216f1acef955aec2eb47cd114
Umair Ashfaq <umairashfaq123321@gmail.com>
2024-01-16 23:26:25 +0500

    ImageUploadingForProfileCreation

--- TaskManagmentSystem(TMS)/Controllers/EmployeeController.cs
+++ TaskManagmentSystem(TMS)/Controllers/EmployeeController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -17,8 +18,42 @@ namespace TaskManagmentSystem_TMS_.Controllers
         [HttpPost] 
         public ActionResult CreateEmployee(Employee employee)
         {
+            //HttpPostedFileBase pb = Request.Files["ProfilePicture"];
 
-            HttpPostedFileBase pb = Request.Files["ProfilePicture01"];
+            Employee employee1 = new Employee();
+            
+            var ProfileImage = employee.ProfilePicture;
+
+            if((ProfileImage.FileName != null) && (ProfileImage.ContentLength > 0))
+            {
+                string ProfileImgExtention = Path.GetExtension(ProfileImage.FileName);
+                string[] allowedExtention = { ".jpg", ".png", ".jpeg", ".svg" };
+
+                if (allowedExtention.Contains(ProfileImgExtention))
+                {
+                    var maxfilesize = 5 * 1024 * 1024; // 5 MB
+                    if(ProfileImage.ContentLength > maxfilesize)
+                    {
+                        return Json("This file is greater than 5MB");
+                    }
+
+                    string fileName = Path.GetFileName(ProfileImage.FileName);
+                    string path = Path.Combine(Server.MapPath("~/Content/Images"),fileName);
+
+                    employee.ProfilePicture.SaveAs(path);
+
+
+                    employee1.ProfilePicture = path;
+
+                }
+
+
+            }
+
+
+
+
+
             return Json("dshfldshf");
         }
     }
